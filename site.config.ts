8a16fefa8daad2dc22b51c914c49c8d23363c58c
Son Le <vansonbmt1993@gmail.com>
2024-05-17 06:21:35 +0700

    Change creator

--- site.config.ts
+++ site.config.ts
@@ -11,7 +11,7 @@ export default siteConfig({
   // basic site info (required)
   name: "Gemini's Blog",
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Son Le',
+  author: 'Gemini',
 
   // open graph metadata (optional)
   description: "Welcome to Gemini's Blog",
